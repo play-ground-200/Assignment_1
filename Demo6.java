b0887d3648d4ce9b9f1a0ab4a1f80dc84eb171f2
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-08-01 23:38:31 +0530

    completed task 6-b

--- Demo6.java
+++ Demo6.java
@@ -1,6 +1,21 @@
 public class Demo6{
     public static void main(String[] args) {
         
-        System.out.printf("\033[41m[   5\033[42m0%%  ]\033[0m");
+        System.out.printf("\033[41m[   5\033[42m0%%  ]\033[0m\n");
+
+        String name1 = "Alice";
+        String name2 = "Bob";
+
+        int age1 = 24;
+        int age2 = 30;
+        String yello = "\033[33m";
+        String reset = "\033[0m";
+
+        System.out.printf("%s+---------------+-----+%s\n", yello, reset);
+        System.out.printf("%1$s|\033[34;1m%3$-15s%2$s%1$s|\033[34;1m%4$-5s%2$s%1$s|%2$s\n",yello, reset, "Name", "Age");
+        System.out.printf("%s+---------------+-----+%s\n", yello, reset);
+        System.out.printf("%1$s|%2$s%3$-15s%1$s|\033[35m%4$ -5d%2$s%1$s|%2$s\n",yello, reset, name1, age1);
+        System.out.printf("%1$s|%2$s%3$-15s%1$s|\033[35m%4$ -5d%2$s%1$s|%2$s\n",yello, reset, name2, age2);
+        System.out.printf("%s+---------------+-----+%s\n", yello, reset);
     }
 } 
