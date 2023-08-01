0149f7fbf69d3534072e74f95f1c6e9e32fc7f83
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-08-01 22:20:13 +0530

    completed task 3

---
+++ demo3.java
@@ -0,0 +1,8 @@
+public class demo3{
+    public static void main(String[] args) {
+        int number1  = 123;
+        int number2  = 456;
+        int number3  = 7890;
+        System.out.printf("\033[34m(%d)\033[0m %d-%d", number1, number2,number3);
+    }
+}
